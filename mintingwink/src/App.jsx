0ef3d8f1b9a1b4ccd1890b9da1b76ce890a02ceb
nishanthspk <spknishanth2004@gmail.com>
2024-09-26 10:10:59 +0530

    postcss

--- mintingwink/src/App.jsx
+++ mintingwink/src/App.jsx
@@ -8,26 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-gray-700 h-screen '>
+      <button className='bg-blue-500 px-6 py-2 rounded-lg '>Connect Wallet</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
