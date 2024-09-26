9ef6908ad60f002c7c1361eff732efb21ea89efe
nishanthspk <spknishanth2004@gmail.com>
2024-09-26 10:36:51 +0530

    cords

--- mintingwink/src/App.jsx
+++ mintingwink/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import BG from './assets/BG.png';
 import './App.css'
 
 function App() {
@@ -9,9 +8,19 @@ function App() {
   return (
     <>
       <div className='bg-gray-700 h-screen '>
-      <button className='bg-blue-500 px-6 py-2 rounded-lg '>Connect Wallet</button>
+      <div className='flex justify-center pt-28'>
+      <button className='bg-blue-500 px-28 py-3 rounded-3xl text-white font-bold'>Connect Wallet</button>
+      </div>
+      <div className='flex justify-center my-10'>
+      <img src={BG} className='h-[200px] w-[200px] ' alt="" />
+      </div>
+      <div className='flex justify-center '>
+      <input type="text" className='px-24 bg-blue-200 py-2 rounded-lg' />
+      </div>
       <div>
-      
+      <div className='flex justify-center my-10'>
+      <button className='bg-blue-500 px-36 py-3 rounded-3xl text-white font-bold'>Mint</button>
+      </div>
       </div>
       </div>
       
