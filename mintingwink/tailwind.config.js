8aa55cee8c9fe60bee6667fe940170cedc2930f0
nishanthspk <spknishanth2004@gmail.com>
2024-09-26 10:37:24 +0530

    gradients

--- mintingwink/tailwind.config.js
+++ mintingwink/tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        customStart: "#06061E",
+        customStart1: "#009EE5",
+        customEnd: "#2070F4",
+        customEnd2: "#A36EFD",
+        customBorder: '#2070F4',
+        customGrayFill: "#1F2138",
+        customGrayStroke: "#384160",
+        customFontColor: "#D8E4FD",
+        customButtonStroke: "#2070F4"
+      },
     },
   },
   plugins: [],
